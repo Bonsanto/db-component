b016265e6cfd6ad03fd6baed5f2f024e50bbf82c
Bonsanto <xbonsx@gmail.com>
2015-05-16 18:03:59 -0430

    Simplified code in JSON class.

--- src/dependencies/JSON.java
+++ src/dependencies/JSON.java
@@ -6,15 +6,14 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class JSON {
-	private static String quote = "\"";
-	protected String json = "{";
+	private final String quote = "\"";
+	private String json = "{";
 
 	public JSON(JSON json) {
 		this.json = json.json;
 	}
 
 	public JSON() {
-		this.json = "{";
 	}
 
 	public String getJson() {
@@ -53,7 +52,7 @@ public class JSON {
 
 	//TODO: FIX A BUG WHEN YOU USE THE JSON CONSTRUCTOR.
 	public void addAttribute(String attribute, Object value) {
-		StringBuilder concat = new StringBuilder();
+		StringBuilder stb = new StringBuilder();
 
 		if (isArray(value)) {
 			int length = Array.getLength(value);
@@ -61,65 +60,65 @@ public class JSON {
 			for (int i = 0; i < length; i++) list[i] = Array.get(value, i);
 			addAttribute(attribute, list);
 		} else {
-			concat.append(json);
-			concat.append(quote);
-			concat.append(attribute);
-			concat.append(quote);
-			concat.append(": ");
-			json = concat.toString();
+			stb.append(json);
+			stb.append(quote);
+			stb.append(attribute);
+			stb.append(quote);
+			stb.append(": ");
+			json = stb.toString();
 
 			if (value instanceof JSON) {
-				concat.append(((JSON) value).json);
-				concat.append(", ");
-				json = concat.toString();
+				stb.append(((JSON) value).json);
+				stb.append(", ");
+				json = stb.toString();
 			} else if (value instanceof String) {
-				concat.append(quote);
-				concat.append(value);
-				concat.append(quote);
-				concat.append(", ");
-				json = concat.toString();
+				stb.append(quote);
+				stb.append(value);
+				stb.append(quote);
+				stb.append(", ");
+				json = stb.toString();
 			} else {
-				concat.append(value);
-				concat.append(", ");
-				json = concat.toString();
+				stb.append(value);
+				stb.append(", ");
+				json = stb.toString();
 			}
 		}
 	}
 
 	public void addAttribute(String attribute, Object[] value) {
-		StringBuilder concat = new StringBuilder();
-		concat.append(json);
-		concat.append(quote);
-		concat.append(attribute);
-		concat.append(quote);
-		concat.append(":[");
-		json = concat.toString();
+		StringBuilder stb = new StringBuilder();
+		stb.append(json);
+		stb.append(quote);
+		stb.append(attribute);
+		stb.append(quote);
+		stb.append(":[");
+		json = stb.toString();
 
 		for (Object aValue : value) {
 			if (aValue instanceof JSON) {
-				concat.append(((JSON) aValue).json);
-				concat.append(", ");
-				json = concat.toString();
+				stb.append(((JSON) aValue).json);
+				stb.append(", ");
+				json = stb.toString();
 			} else if (aValue instanceof String) {
-				concat.append(quote);
-				concat.append(aValue);
-				concat.append(quote);
-				concat.append(", ");
-				json = concat.toString();
+				stb.append(quote);
+				stb.append(aValue);
+				stb.append(quote);
+				stb.append(", ");
+				json = stb.toString();
 			} else {
-				concat.append(aValue);
-				concat.append(", ");
-				json = concat.toString();
+				stb.append(aValue);
+				stb.append(", ");
+				json = stb.toString();
 			}
 		}
 		if (value.length > 0) {
-			concat.delete(0, concat.length());
-			concat.append(json.substring(0, json.length() - 2));
-			concat.append("], ");
-			json = concat.toString();
+			stb.delete(0, stb.length());
+			stb.append(json.substring(0, json.length() - 2));
+			stb.append("], ");
+			json = stb.toString();
 		} else {
-			concat.append("], ");
-			json = concat.toString();
+			stb.append("], ");
+			json = stb.toString();
 		}
 
 	}
